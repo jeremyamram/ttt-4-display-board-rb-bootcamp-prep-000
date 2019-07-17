fb6d9d77ef3fefc357b240b8b90ab4d98a5bf02d
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-07-17 16:04:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,7 @@
 def display_board (baracuda)
-puts " #{baracuda[0]}  |{baracuda[1]} |{baracuda[2]} "
+puts " #{baracuda[0]} | #{baracuda[1]} | #{baracuda[2]} "
 puts "-----------"
-puts " {baracuda[0]} |   |   "
+puts " #{baracuda[3]} | #{baracuda[4]} | #{baracuda[5]} "
 puts "-----------"
-puts "   |   |   "
+puts " #{baracuda[6]} | #{baracuda[7]} | #{baracuda[8]} "
 end
-board =
